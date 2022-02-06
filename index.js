1909cb86ed8aa25a1948951d02384c15c79035c0
FFMC <92620179+FlammableFlowMC@users.noreply.github.com>
2022-02-06 14:00:07 -0800

    added index.js, script for the first one

---
+++ index.js
@@ -0,0 +1,14 @@
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + '' + lastName)
+}
+
+greet('FFMC', ' FFMC');
+
+//Caluclating
+function square(number) {
+    return number * number
+}
+
+let number = square(2);
+console.log(number);
